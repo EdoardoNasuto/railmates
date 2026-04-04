095b54f4c0bd2061c2afa6574cbc5e9c10d1a4d6
Edoardo Nasuto <edoardo.nasuto@gmail.com>
2026-04-04 14:15:56 +0200

    ⬆️ Update Nowa version to 3.6.5 and removes unnecessary permissions

--- android/app/src/main/kotlin/com/example/railmates/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/railmates/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.railmates
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
